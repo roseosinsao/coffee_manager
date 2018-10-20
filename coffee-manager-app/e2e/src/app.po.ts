dda9afd09f423ac6dfbb8127cee143258c552c29
roseosinsao <roseosinsao@gmail.com>
2018-10-20 10:10:04 +0800

    coffee-manager-app: upgrade angular2 to angular7

--- coffee-manager-app/e2e/src/app.po.ts
+++ coffee-manager-app/e2e/src/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class CoffeeManagerAppPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
--- coffee-manager-app/src/app/app.module.ts
+++ coffee-manager-app/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,8 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
